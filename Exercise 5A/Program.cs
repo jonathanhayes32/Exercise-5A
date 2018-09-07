44cef392bd97a611f22a771591dc2a71b3de0789
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-09-07 10:35:54 -0400

    git commit(ADD) to command line

--- Exercise 5A/Program.cs
+++ Exercise 5A/Program.cs
@@ -10,6 +10,23 @@ namespace Exercise_5A
     {
         static void Main(string[] args)
         {
+            //build a farm
+            Mouse Cheryll = new Mouse();
+            Chicken Martha = new Chicken();
+            Goat Spot = new Goat();
+            Sheep Beatrice = new Sheep();
+
+            Console.WriteLine("Hello and welcome to my farm!");
+            Console.WriteLine("Let me introduce you to our animals");
+            Console.WriteLine();
+
+            //Call the animals
+            Cheryll.speak();
+            Martha.speak();
+            Spot.speak();
+            Beatrice.speak();
+            Console.ReadLine();
+
         }
     }
 }
